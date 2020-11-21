53b461eb16d75fa1a61461795bef92ae73c711f2
George Harris <g.harris@reply.eu>
2020-11-21 16:50:00 +0000

    Try and sort out namespace

--- ClassLibrary1/IAccountService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bank
-{
-    public interface IAccountService
-    {
-        public void Deposit(int amount);
-        public void Withdraw(int amount);
-        public void PrintStatement();
-    }
-}
