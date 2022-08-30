b8fb1b912cd4d814026a986f3f5639782dfb830e
George Harris <citizenharris@gmail.com>
2022-08-30 11:26:32 +0100

    Use `MockBehaviour.Strict` to verify prints in order

--- AccountServiceTests/AccountServiceAcceptance.cs
+++ AccountServiceTests/AccountServiceAcceptance.cs
@@ -11,30 +11,36 @@ public class AccountServiceAcceptance
     public void GivenHistoryAccountServicePrintsStatement()
     {
         // Arrange
-        var printer = new Mock<IPrinter>();
+        var printer = new Mock<IPrinter>(MockBehavior.Strict);
         var statementPrinter = new StatementPrinter(printer.Object);
         var dateTimeHandler = new Mock<IDateTimeHandler>();
 
         var dateTimeQueue = new Queue<DateTime>(new[]
         {
-            new DateTime(2012, 01, 10), 
-            new DateTime(2012, 01, 13), 
+            new DateTime(2012, 01, 10),
+            new DateTime(2012, 01, 13),
             new DateTime(2012, 01, 14)
         });
 
         dateTimeHandler
             .Setup(dth => dth.Now())
             .Returns(dateTimeQueue.Dequeue);
-
         var repo = new TransactionRepository(dateTimeHandler.Object);
+
+        var sequence = new MockSequence();
+        printer.InSequence(sequence).Setup(c => c.Print(StatementHeader));
+        printer.InSequence(sequence).Setup(c => c.Print(Line1));
+        printer.InSequence(sequence).Setup(c => c.Print(Line2));
+        printer.InSequence(sequence).Setup(c => c.Print(Line3));
+        
         var account = new AccountService(statementPrinter, repo);
-            
+
         // Act
         account.Deposit(1000);
         account.Deposit(2000);
         account.Withdraw(500);
         account.PrintStatement();
-            
+
         // Assert
         printer.Verify(c => c.Print(StatementHeader));
         printer.Verify(c => c.Print(Line1));
